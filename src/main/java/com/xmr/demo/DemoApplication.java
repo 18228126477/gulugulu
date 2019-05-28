1b701fe564adcabf1e0b46d629ded44b27fcd9e4
p <1142102731@qq.com>
2019-05-28 09:42:39 +0800

    修改静态资源路径，并且让项目能在TOMCAT上部署

--- src/main/java/com/xmr/demo/DemoApplication.java
+++ src/main/java/com/xmr/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.xmr.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -10,6 +11,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @ServletComponentScan
 @MapperScan("com.xmr.demo.dao")
 public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
