fee1119279bf2bbffd78c7d61ed9a783e61340e5
p <1142102731@qq.com>
2019-09-11 17:39:22 +0800

    新增字节跳动链表题

--- src/main/java/com/xmr/demo/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/xmr/demo/common/interceptor/LoginInterceptor.java
@@ -1,32 +1,54 @@
 package com.xmr.demo.common.interceptor;
 
+import com.xmr.demo.dao.UserMapper;
+import com.xmr.demo.domain.User;
+import com.xmr.demo.untils.redis.RedisUntil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    public RedisUntil redisUntil;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = getCookie(cookies);
         if(session.getAttribute("user") != null){
             return true;
+        }else if(cookie != null){
+            Integer id = (Integer) redisUntil.get(cookie.getValue());
+            User user = userMapper.findById(id);
+            session.setAttribute("user",user);
+            return true;
         }
-        response.sendRedirect("/login/index");
+        response.sendRedirect(request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+"/login/index");
         return false;
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+    private Cookie getCookie(Cookie[] cookies){
+        Cookie cookie = null;
+        if(cookies ==null){
+            return null;
+        }
+        for(Cookie coo:cookies){
+            if("loginToken".equals(coo.getName())){
+                cookie = coo;
+            }
+        }
+        return cookie;
     }
 }
