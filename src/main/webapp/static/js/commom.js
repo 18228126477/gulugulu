f6cb7ce12f0a757463f11dc182dd97c6285df66b
p <1142102731@qq.com>
2019-10-14 10:37:02 +0800

    httpclient

---
+++ src/main/webapp/static/js/commom.js
@@ -0,0 +1,11 @@
+//vue and bootstrap
+document.write("<link rel='stylesheet' type='text/css' href='../static/bootstrap/css/bootstrap.css'/>\n" +
+    "    <script src='../static/bootstrap/js/bootstrap.js'></script>\n" +
+    "    <script src='../static/js/vue.js'></script>\n" +
+    "    <script src='../static/js/vue-resource.js'></script>\n" +
+    "    <script src='../static/js/vue-router.min.js'></script>");
+// HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries
+//WARNING: Respond.js doesn't work if you view the page via file:
+//if lt IE 9
+document.write("<script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script>\n" +
+    "    <script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script>");
--- src/test/java/com/xmr/demo/DemoOrdinaryTests.java
+++ src/test/java/com/xmr/demo/DemoOrdinaryTests.java
@@ -2,6 +2,9 @@ package com.xmr.demo;
 
 public class DemoOrdinaryTests {
     public static void main(String[] args){
-
+        int max=1<<16,min=1;
+        long randomNum = System.currentTimeMillis();
+        int ran = (int) (randomNum%(max-min)+min);
+        System.out.println(ran);
     }
 }
