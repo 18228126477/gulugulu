70a6b985dac50269b060e4b3e9fe5aab6b5fc141
Administrator <1142102731@qq.com>
2020-02-20 16:18:05 +0800

    新增springfox接口开发工具

--- src/main/java/com/xmr/demo/common/filter/ControllerFilter.java
+++ src/main/java/com/xmr/demo/common/filter/ControllerFilter.java
@@ -36,6 +36,13 @@ public class ControllerFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletRequest;
+        // response.setHeader("Access-Control-Allow-Origin",reqs.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         logger.info(request.getRequestURI());
         filterChain.doFilter(servletRequest,servletResponse);
     }
--- src/test/java/com/xmr/demo/DemoOrdinaryTests.java
+++ src/test/java/com/xmr/demo/DemoOrdinaryTests.java
@@ -1,10 +1,33 @@
 package com.xmr.demo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DemoOrdinaryTests {
     public static void main(String[] args){
-        int max=1<<16,min=1;
-        long randomNum = System.currentTimeMillis();
-        int ran = (int) (randomNum%(max-min)+min);
-        System.out.println(ran);
+        int i = comparePastDate("2019-11-08", "2019-11-01");
+        System.out.println(i);
+    }
+
+    public static int comparePastDate(String endDate,String startDate){
+        int day=0;
+        try {
+            //设置转换的日期格式
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            //开始时间
+            Date oldDate = sdf.parse(startDate);
+            //结束时间
+            Date newDate = sdf.parse(endDate);
+            //得到相差的天数 betweenDate
+            long betweenDate = (newDate.getTime() - oldDate.getTime())/(60*60*24*1000);
+            //打印控制台相差的天数
+            day = (int)betweenDate+1;
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return day;
     }
 }
