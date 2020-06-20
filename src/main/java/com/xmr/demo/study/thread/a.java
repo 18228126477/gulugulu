0f6d515d373f5edd9d2d6bcaa5356e6310862e21
Administrator <1142102731@qq.com>
2020-06-20 10:05:51 +0800

    spi

--- src/main/java/com/xmr/demo/study/thread/a.java
+++ src/main/java/com/xmr/demo/study/thread/a.java
@@ -9,26 +9,26 @@ public class a {
         //通俗一点的说 就是 使用CountDownLatch 时  几个线程使用同一个CountDownLatch对象 有一个线程调用wait方法
         //其他线程使用countDown()方法，当最后计数器减到0的时候，wait调用处 继续往下执行；
         //CountDownLatch类只提供了一个构造器：
-        final CountDownLatch latch = new CountDownLatch(2);
-        for(int i=0;i<=1;i++){
-            new Thread(() -> {
-                try{
-                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
-                    Thread.sleep(3000);
-                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
-                    latch.countDown();
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }
-            }).start();
-        }
-        try {
-            System.out.println("等待2个子线程执行完毕...");
-            latch.await();
-            System.out.println("2个子线程已经执行完毕");
-            System.out.println("继续执行主线程");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        final CountDownLatch latch = new CountDownLatch(2);
+//        for(int i=0;i<=1;i++){
+//            new Thread(() -> {
+//                try{
+//                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+//                    Thread.sleep(3000);
+//                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+//                    latch.countDown();
+//                }catch(InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//            }).start();
+//        }
+//        try {
+//            System.out.println("等待2个子线程执行完毕...");
+//            latch.await();
+//            System.out.println("2个子线程已经执行完毕");
+//            System.out.println("继续执行主线程");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
