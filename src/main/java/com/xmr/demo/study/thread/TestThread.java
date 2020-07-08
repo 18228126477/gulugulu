ba82bb6d599d44dbbb097bca606fd9e65f3d7df7
Administrator <1142102731@qq.com>
2020-07-08 14:39:52 +0800

    修改

---
+++ src/main/java/com/xmr/demo/study/thread/TestThread.java
@@ -0,0 +1,64 @@
+package com.xmr.demo.study.thread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestThread{
+    public static void main(String[] args){
+        //CountDownLatch类位于java.util.concurrent包下，利用它可以实现类似计数器的功能。比如有一个任务A，
+        //它要等待其他4个任务执行完毕之后才能执行，此时就可以利用CountDownLatch来实现这种功能了。
+        //通俗一点的说 就是 使用CountDownLatch 时  几个线程使用同一个CountDownLatch对象 有一个线程调用wait方法
+        //其他线程使用countDown()方法，当最后计数器减到0的时候，wait调用处 继续往下执行；
+        //CountDownLatch类只提供了一个构造器：
+        final CountDownLatch latch = new CountDownLatch(2);
+        for(int i=0;i<=1;i++){
+            new Thread(() -> {
+                try{
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+        try {
+            System.out.println("等待2个子线程执行完毕...");
+            latch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] ints = twoSum(nums, target);
+        for(int i=0;i<ints.length;i++){
+            System.out.println(ints[i]);
+        }
+        new Thread(() -> System.out.println("1")){
+            @Override
+            public void run() {
+                System.out.println("2");
+            }
+        }.start();
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int value = target-nums[i];
+                System.out.println(value);
+                System.out.println(nums[j]);
+                if(value==nums[j]){
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
