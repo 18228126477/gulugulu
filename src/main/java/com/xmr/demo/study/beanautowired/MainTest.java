dc9f53166c5ffd93b7eb3762491a1c7622840873
Administrator <1142102731@qq.com>
2020-07-17 10:12:46 +0800

    学习profile

--- src/main/java/com/xmr/demo/study/beanautowired/MainTest.java
+++ src/main/java/com/xmr/demo/study/beanautowired/MainTest.java
@@ -3,12 +3,19 @@ package com.xmr.demo.study.beanautowired;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
+
 public class MainTest {
 
     @Test
     public void test1(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfigOfAutowired.class);
-        BookService bookService = context.getBean(BookService.class);
-        bookService.print();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.getEnvironment().setActiveProfiles("test");
+        context.register(MainConfigProfile.class);
+        context.refresh();
+        String[] names = context.getBeanNamesForType(DataSource.class);
+        for(String name:names){
+            System.out.println(name);
+        }
     }
 }
